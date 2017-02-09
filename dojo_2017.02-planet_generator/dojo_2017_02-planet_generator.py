987c97db9db8bd6c02536894251b617b7740ffc8
Stanislav Schmidt <stanislav.schmidt@outlook.com>
2017-02-09 21:17:18 +0000

    progress

--- dojo_2017.02-planet_generator/dojo_2017_02-planet_generator.py
+++ dojo_2017.02-planet_generator/dojo_2017_02-planet_generator.py
@@ -6,62 +6,91 @@ import math
 
 
 background_colour = (0, 0, 0)
-particle_color = (255, 255, 255)
-sun_color = (255, 0, 0)
+n_particles = 100
+n_suns = 3
 (width, height) = (600, 400)
 
 
 class Sun():
-    def __init__(self, x, y, G):
-        self.x = x
-        self.y = y
-        self.G = G
+    def __init__(self, x, y, M):
+        self.x = int(x)
+        self.y = int(y)
+        self.color = (255, 0, 0) if M > 0 else (0, 0, 255)
+        self.r = int(1 + abs(M))
+        self.M = M
 
     def draw(self, screen):
-        pygame.draw.circle(screen, sun_color,
-                           (int(self.x), int(self.y)), self.G, 0)
+        pygame.draw.circle(screen, self.color,
+                           (self.x, self.y), self.r, 0)
 
 
 class Particle():
-    def __init__(self, x, y, sun):
+    def __init__(self, x, y, m, suns):
         self.x = x
         self.y = y
         self.vx = random.random() * 4 - 2
         self.vy = random.random() * 4 - 2
+        # self.vx = self.vy = 0
         self.ax = 0
-        self.ay = 0.2
-        self.sun = sun
+        self.ay = 0
+        self.r = int(1 + abs(m) / 10)
+        self.m = m
+        self.suns = suns
+        self.dead = False
+        self.color = (255, 255, 255)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, particle_color,
-                           (int(self.x), int(self.y)), 2, 0)
+        pygame.draw.circle(screen, self.color,
+                           (int(self.x), int(self.y)), self.r, 0)
 
     def update(self):
+        if self.dead:
+            return
+
         self.x += self.vx
         self.y += self.vy
         self.vx += self.ax
         self.vy += self.ay
-        dx = self.x - sun.x
-        dy = self.y - sun.y
-        d = math.sqrt(dx * dx + dy * dy)
-        alpha = math.atan2(dy, dx)
-        self.ax = -sun.G * math.cos(alpha) / d
-        self.ay = -sun.G * math.sin(alpha) / d
+
+        self.ax = 0
+        self.ay = 0
+        for sun in self.suns:
+            dx = self.x - sun.x
+            dy = self.y - sun.y
+            d = math.sqrt(dx * dx + dy * dy)
+            if d < self.r + sun.r:
+                self.dead = True
+                self.color = (255, 255, 0)
+            alpha = math.atan2(dy, dx)
+            self.ax += -0.5 * self.m * sun.M * math.cos(alpha) / (d * d)
+            self.ay += -0.5 * self.m * sun.M * math.sin(alpha) / (d * d)
 
 
 sun = Sun(width / 2, height / 2, 5)
+suns = []
+for i in range(n_suns):
+    x = random.random() * width / 2 + width / 4
+    y = random.random() * height / 2 + width / 4
+    M = random.random() * 30 - 15
+    suns.append(Sun(x, y, M))
+
 particles = []
-for i in range(150):
+for i in range(n_particles):
     x = random.random() * width
     y = random.random() * height
-    particles.append(Particle(x, y, sun))
+    m = random.random() * 30
+    particles.append(Particle(x, y, m, suns))
 
 
 def draw_main(screen):
-    sun.draw(screen)
+    for sun in suns:
+        sun.draw(screen)
+
     for p in particles:
         p.draw(screen)
         p.update()
+        if p.dead:
+            particles.remove(p)
 
 
 def main():
