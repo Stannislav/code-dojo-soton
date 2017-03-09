dd7f86edc12d142fe77d5fdc3f101b43923e9352
Stanislav Schmidt <stanislav.schmidt@outlook.com>
2017-03-09 19:47:14 +0000

    progress

--- dojo_2016.12-snow_storm_generator/snow_storm_gen.py
+++ dojo_2016.12-snow_storm_generator/snow_storm_gen.py
@@ -4,108 +4,138 @@ import pygame
 import random
 import math
 
-background_colour = (0,0,0)
+background_colour = (0, 0, 0)
 (width, height) = (600, 400)
-wind = random.random()*2-1
+wind = random.random() * 2 - 1
 def_size = 5
 
+
 class Flake():
-	def __init__(self, screen, x_y, size):
-		self.screen = screen
-		self.x, self.y = x_y
-		self.size = size
-		self.colour = (255, 255, 255)
-		self.thickness = 1
-		self.speed = 2+random.random()
-		self.wind = wind
-		self.stopped = False
-		self.intspeed=random.random()*0.2-0.1
-		self.intangle=0
-		self.wobbleangle=0
-		self.wobbleamp=1
-		self.wobblespeed=0.3
+    def __init__(self, screen, x_y, size):
+        self.screen = screen
+        self.x, self.y = x_y
+        self.size = size
+        self.colour = (255, 255, 255)
+        self.thickness = 1
+        self.speed = 2 + random.random()
+        self.wind = wind
+        self.stopped = False
+        self.intspeed = random.random() * 0.2 - 0.1
+        self.intangle = 0
+        self.wobbleangle = 0
+        self.wobbleamp = 1
+        self.wobblespeed = 0.3
 
-	def display(self):
-		pygame.draw.circle(self.screen, self.colour, (int(self.x), int(self.y)), self.size/2, self.thickness)
-		for n in range(3):
-			angle = n*math.pi/3 + self.intangle
-			pygame.draw.line(self.screen, self.colour,
-				(int(self.x-self.size*math.cos(angle)), int(self.y-self.size*math.sin(angle))),
-				(int(self.x+self.size*math.cos(angle)), int(self.y+self.size*math.sin(angle))), self.thickness)
+    def display(self):
+        pygame.draw.circle(self.screen, self.colour, (int(
+            self.x), int(self.y)), self.size / 2, self.thickness)
+        for n in range(3):
+            angle = n * math.pi / 3 + self.intangle
+            pygame.draw.line(self.screen, self.colour,
+                             (int(self.x - self.size * math.cos(angle)),
+                              int(self.y - self.size * math.sin(angle))),
+                             (int(self.x + self.size * math.cos(angle)),
+                              int(self.y + self.size * math.sin(angle))),
+                             self.thickness)
 
-	def move(self):
-		self.intangle += self.intspeed
-		self.angle = 3/2*math.pi + self.wind*math.pi/4
-		self.wobbleangle += self.wobblespeed
-		self.x += math.sin(self.angle) * self.speed + math.sin(self.wobbleangle)*self.wobbleamp
-		self.y -= math.cos(self.angle) * self.speed
+    def move(self):
+        self.intangle += self.intspeed
+        self.angle = 3 / 2 * math.pi + self.wind * math.pi / 4
+        self.wobbleangle += self.wobblespeed
+        self.x += math.sin(self.angle) * self.speed + \
+            math.sin(self.wobbleangle) * self.wobbleamp
+        self.y -= math.cos(self.angle) * self.speed
+
 
 def change_wind():
-	global wind
-	if random.randint(0,100) == 0:
-		wind = random.random()*2-1
-		for flake in my_flakes:
-			flake.wind = wind
+    global wind
+    if random.randint(0, 100) == 0:
+        wind = random.random() * 2 - 1
+        for flake in my_flakes:
+            flake.wind = wind
+
 
 def draw_snowman(screen):
-	pygame.draw.circle(screen, (255,255,255), (width/4, height-20), 40, 0)
-	pygame.draw.circle(screen, (255,255,255), (width/4, height-75), 30, 0)
-	pygame.draw.circle(screen, (255,255,255), (width/4, height-115), 20, 0)
-	pygame.draw.line(screen, (100,50,0),(width/4-20, height-85),(width/4-50, height-100), 3)
-	pygame.draw.line(screen, (100,50,0),(width/4+20, height-85),(width/4+50, height-100), 3)
-	pygame.draw.circle(screen, (0,0,0), (width/4-10, height-120), 2, 0)
-	pygame.draw.circle(screen, (0,0,0), (width/4+10, height-120), 2, 0)
+    pygame.draw.circle(screen, (255, 255, 255),
+                       (width / 4, height - 20), 40, 0)
+    pygame.draw.circle(screen, (255, 255, 255),
+                       (width / 4, height - 75), 30, 0)
+    pygame.draw.circle(screen, (255, 255, 255),
+                       (width / 4, height - 115), 20, 0)
+    pygame.draw.line(
+        screen,
+        (100, 50, 0),
+        (width / 4 - 20, height - 85),
+        (width / 4 - 50, height - 100),
+        3)
+    pygame.draw.line(
+        screen,
+        (100, 50, 0),
+        (width / 4 + 20, height - 85),
+        (width / 4 + 50, height - 100), 3)
+    pygame.draw.circle(screen, (0, 0, 0), (width / 4 - 10, height - 120), 2, 0)
+    pygame.draw.circle(screen, (0, 0, 0), (width / 4 + 10, height - 120), 2, 0)
+
 
 def draw_main(screen):
-	global my_flakes
-	# change wind
-	change_wind()
-	# add new flakes
-	if random.randint(0,1) == 0:
-		my_flakes += [Flake(screen, (random.randint(0,3*width)-width,-10-random.randint(0,20)), def_size) for i in range(5)]
-	# draw all flakes
-	for i,flake in enumerate(my_flakes):
-		if flake.y > height:
-			del my_flakes[i]
-			continue
-		flake.move()
-		flake.display()
-	# draw the snowman
-	draw_snowman(screen)
+    global my_flakes
+    # change wind
+    change_wind()
+    # add new flakes
+    if random.randint(0, 1) == 0:
+        my_flakes += [Flake(
+            screen,
+            (random.randint(0, 3 * width) - width,
+             -10 - random.randint(0, 20)),
+            def_size) for i in range(5)]
+    # draw all flakes
+    for i, flake in enumerate(my_flakes):
+        if flake.y > height:
+            del my_flakes[i]
+            continue
+        flake.move()
+        flake.display()
+    # draw the snowman
+    draw_snowman(screen)
+
 
 my_flakes = []
+
+
 def main():
-# initialize pygame
-	pygame.init()
-	screen = pygame.display.set_mode((width, height), pygame.DOUBLEBUF)
-	pygame.display.set_caption('Snow Flakes')
+    # initialize pygame
+    pygame.init()
+    screen = pygame.display.set_mode((width, height), pygame.DOUBLEBUF)
+    pygame.display.set_caption('Snow Flakes')
 
 # some settings
-	number_of_particles = 50
+    number_of_particles = 50
 
 # generate initial flakes
-	for n in range(number_of_particles):
-		x = random.randint(0,3*width)-width
-		flake = Flake(screen, (x,0), def_size)
-		my_flakes.append(flake)
+    for n in range(number_of_particles):
+        x = random.randint(0, 3 * width) - width
+        flake = Flake(screen, (x, 0), def_size)
+        my_flakes.append(flake)
 
 # main loop
-	running = True
-	while running:
-# events for window closing
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-			elif event.type == pygame.KEYDOWN:
-				keys = pygame.key.get_pressed()
-				mods = pygame.key.get_mods()
-				if keys[pygame.K_ESCAPE] or (keys[pygame.K_w] and mods&pygame.KMOD_META):
-					running = False
+    running = True
+    while running:
+        # events for window closing
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                mods = pygame.key.get_mods()
+                if keys[pygame.K_ESCAPE] or \
+                   (keys[pygame.K_w] and mods & pygame.KMOD_META):
+                    running = False
 # drawing routines
-		screen.fill(background_colour)
-		draw_main(screen)
-		pygame.display.flip()
-	pygame.quit()
+        screen.fill(background_colour)
+        draw_main(screen)
+        pygame.display.flip()
+    pygame.quit()
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- dojo_2017.02-planet_generator/dojo_2017_02-planet_generator.py
+++ dojo_2017.02-planet_generator/dojo_2017_02-planet_generator.py
@@ -6,124 +6,161 @@ import math
 
 
 background_colour = (0, 0, 0)
-n_particles = 100
-n_suns = 3
-p_new = None
-c_down = None
 (width, height) = (800, 600)
+clock = None
+fps = 60
+
+# n_planets = 100
+# n_stars = 1
+# p_new = None
+# c_down = None
+
+# G = 0.5
 
 
-class Sun():
-    def __init__(self, x, y, M):
-        self.x = int(x)
-        self.y = int(y)
-        self.color = (255, 0, 0) if M > 0 else (0, 0, 255)
-        self.r = int(math.ceil(math.sqrt(abs(M))))
-        self.M = M
-
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color,
-                           (self.x, self.y), self.r, 0)
-
-
-class Particle():
-    def __init__(self, x, y, m, suns):
+class Particle(object):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.vx = random.random() * 4 - 2
-        self.vy = random.random() * 4 - 2
-        # self.vx = self.vy = 0
-        self.r = int(math.ceil(math.sqrt(abs(m))))
-        self.m = m
-        self.suns = suns
-        self.dead = False
+        self.r = 1
         self.color = pygame.Color(255, 255, 255)
-        self.prev_pos = []
-        self.trail = 5
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color,
                            (int(self.x), int(self.y)), self.r, 0)
 
-        for i, p in enumerate(self.prev_pos):
-            c = self.color - pygame.Color(*[255-255/self.trail*i]*3)
-            pygame.draw.circle(screen, c,
-                               (int(p[0]), int(p[1])), self.r, 0)
+
+class Bullet(Particle):
+    def __init__(self, x, y):
+        super(Bullet, self).__init__(x, y)
+        self.v = -5
+        self.lifetime = 90 - random.randint(0, 20)
+        self.dead = False
 
     def update(self):
-        if self.dead:
-            return
+        self.y = self.y + self.v
+        self.lifetime = self.lifetime - 1
 
-        ax = 0
-        ay = 0
-        for sun in self.suns:
-            dx = self.x - sun.x
-            dy = self.y - sun.y
-            d = math.sqrt(dx * dx + dy * dy)
-            if d < self.r + sun.r:
-                self.dead = True
-                self.color = (255, 255, 0)
-            alpha = math.atan2(dy, dx)
-            ax += -0.5 * self.m * sun.M * math.cos(alpha) / (d * d)
-            ay += -0.5 * self.m * sun.M * math.sin(alpha) / (d * d)
+    def explode(self):
+        self.dead = True
 
-        self.vx += ax
-        self.vy += ay
-        self.prev_pos.append((self.x, self.y))
-        if len(self.prev_pos) > self.trail:
-            self.prev_pos = self.prev_pos[-self.trail:]
-        self.x += self.vx
-        self.y += self.vy
-
-        # die if too far away
-        if abs(self.x - width/2) > 1000 or abs(self.y - height/2) > 1000:
-            self.dead = True
+# class Star(Particle):
+#     def __init__(self, x, y, M):
+#         super(Star, self).__init__(x, y, M)
+#         self.color = (255, 0, 0) if M > 0 else (0, 0, 255)
 
 
-suns = []
-for i in range(n_suns):
-    x = random.random() * width / 2 + width / 4
-    y = random.random() * height / 2 + height / 4
-    M = random.random() * 100 + 50
-    if random.random() < 0.5:
-        M = -M
-    suns.append(Sun(x, y, M))
+# class Planet(Particle):
+#     def __init__(self, x, y, m, stars):
+#         super(Planet, self).__init__(x, y, m)
+#         self.vx = random.random() * 4 - 2
+#         self.vy = random.random() * 4 - 2
+#         self.stars = stars
+#         self.dead = False
+#         self.prev_pos = []
+#         self.trail = 5
 
-particles = []
-for i in range(n_particles):
-    x = random.random() * width
-    y = random.random() * height
-    m = random.random() * 10
-    particles.append(Particle(x, y, m, suns))
+#     def draw(self, screen):
+#         super(Planet, self).draw(screen)
+
+#         # draw the trail
+#         for i, p in enumerate(self.prev_pos):
+#             c_decrease = int(255 - 255 / self.trail * i)
+#             c = self.color - pygame.Color(*[c_decrease] * 3)
+#             pygame.draw.circle(screen, c,
+#                                (int(p[0]), int(p[1])), self.r, 0)
+
+#     def update(self):
+#         if self.dead:
+#             return
+
+#         # compute total acceleration to all stars
+#         ax = ay = 0
+#         for star in self.stars:
+#             # compute distance to star
+#             dx = self.x - star.x
+#             dy = self.y - star.y
+#             d = math.sqrt(dx * dx + dy * dy)
+
+#             # check for collisions
+#             if d < self.r + star.r:
+#                 self.dead = True
+
+#             # update acceleration
+#             a = math.atan2(dy, dx)
+#             ax += - G * self.m * star.m * math.cos(a) / (d * d)
+#             ay += - G * self.m * star.m * math.sin(a) / (d * d)
+
+#         # update velocity
+#         self.vx += ax
+#         self.vy += ay
+
+#         # store position history for trail
+#         self.prev_pos.append((self.x, self.y))
+#         if len(self.prev_pos) > self.trail:
+#             self.prev_pos = self.prev_pos[-self.trail:]
+
+#         # update position
+#         self.x += self.vx
+#         self.y += self.vy
+
+#         # die if too far away
+#         if abs(self.x - width / 2) > 1000 or abs(self.y - height / 2) > 1000:
+#             self.dead = True
+
+
+# stars = []
+# for i in range(n_stars):
+#     x = random.random() * width / 2 + width / 4
+#     y = random.random() * height / 2 + height / 4
+#     M = random.random() * 100 + 50
+#     if random.random() < 0.5:
+#         M = -M
+#     stars.append(Star(x, y, M))
+
+# planets = []
+# for i in range(n_planets):
+#     x = random.random() * width
+#     y = random.random() * height
+#     m = random.random() * 10
+#     planets.append(Planet(x, y, m, stars))
+
+
+# x = 50
+bullets = [Bullet(random.randint(0, width), height)]
 
 
 def draw_main(screen):
-    for sun in suns:
-        sun.draw(screen)
+    clock.tick(fps)
 
-    for p in particles:
-        p.draw(screen)
-        p.update()
-        if p.dead:
-            particles.remove(p)
+    # pygame.draw.line(screen, (0, 255, 255), (100, 100), (400, 400))
+    # pygame.draw.circle(screen, (255, 255, 255), (300, 300), 4, 0)
 
-    if p_new:
-        p_new.x, p_new.y = pygame.mouse.get_pos()
-        p_new.draw(screen)
-        pygame.draw.line(screen, (0, 255, 255), c_down, pygame.mouse.get_pos())
+    if random.random() < 0.1:
+        bullets.append(Bullet(random.randint(0, width), height))
+
+    for b in bullets:
+        if not b.dead:
+            b.update()
+            b.draw(screen)
+            if b.lifetime == 0:
+                b.explode()
+
+    # pygame.draw.line(screen, (0, 255, 255), c_down, pygame.mouse.get_pos())
 
 
 def main():
+    global clock
     # initialize pygame
     pygame.init()
     screen = pygame.display.set_mode((width, height), pygame.DOUBLEBUF)
-    pygame.display.set_caption('Planets')
-    global p_new, c_down
+    pygame.display.set_caption('Fireworks')
+    clock = pygame.time.Clock()
 
     # main loop
     running = True
     while running:
-        # events for window closing
+        # event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -133,16 +170,16 @@ def main():
                 if keys[pygame.K_ESCAPE] or \
                    (keys[pygame.K_w] and mods & pygame.KMOD_META):
                     running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                c_down = pygame.mouse.get_pos()
-                m = random.random() * 30
-                p_new = Particle(c_down[0], c_down[1], m, suns)
-            elif event.type == pygame.MOUSEBUTTONUP:
-                c_up = pygame.mouse.get_pos()
-                p_new.vx = (c_up[0] - c_down[0]) / 20
-                p_new.vy = (c_up[1] - c_down[1]) / 20
-                particles.append(p_new)
-                p_new = None
+            # elif event.type == pygame.MOUSEBUTTONDOWN:
+            #     c_down = pygame.mouse.get_pos()
+            #     m = random.random() * 30
+            #     p_new = Planet(c_down[0], c_down[1], m, stars)
+            # elif event.type == pygame.MOUSEBUTTONUP:
+            #     c_up = pygame.mouse.get_pos()
+            #     p_new.vx = (c_up[0] - c_down[0]) / 20
+            #     p_new.vy = (c_up[1] - c_down[1]) / 20
+            #     planets.append(p_new)
+            #     p_new = None
 
         # drawing routines
         screen.fill(background_colour)
